f4d27821b3bc063ff0160bf668cab349cca49366
Evgenii Rychkov <evgeniirychkov@Evgeniis-MacBook-Pro.local>
2024-07-15 20:11:15 -0700

    Add new todo

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,6 @@
 import TodoListItem from './TodoListItem.jsx'
 
-const todoList = [
-  {
-    id: 1,
-    title: 'Complete assignment',
-  },
-  {
-    id: 2,
-    title: 'Сall for interview',
-  },
-  {
-    id: 3,
-    title: 'Feed the cats',
-  },
-];
-
-const TodoList = () => (
+const TodoList = ({todoList}) => (
   <ul>
     {todoList.map(item => ( <TodoListItem key={item.id} item={item} /> ))}
   </ul>
