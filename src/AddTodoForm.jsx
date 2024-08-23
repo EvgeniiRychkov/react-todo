f92cc1f0bd0a93de23311369a4d51f9c2ad8e1c5
Evgenii Rychkov <evgeniirychkov@Evgeniis-MacBook-Pro.local>
2024-08-23 12:11:31 -0700

    [lesson_1_9] Add route New

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import InputWithLabel from './InputWithLabel.jsx'
 
 const AddTodoForm = ({onAddTodo}) => {
   const [todoTitle, setTodoTitle] = React.useState("");
+  const navigate = useNavigate();
 
   const handleTitleChange = (event) => {
     const newTodoTitle = event.target.value;
@@ -13,10 +15,17 @@ const AddTodoForm = ({onAddTodo}) => {
     event.preventDefault();
     setTodoTitle("");
     onAddTodo(todoTitle);
+    navigate('/');
   }
 
   return (
     <form onSubmit={handleAddTodo}>
+      <Link to="/">
+        <button type="button">Back</button>
+      </Link>
+
+      <h1>New Todo List</h1>
+
       <InputWithLabel todoTitle={todoTitle} handleTitleChange={handleTitleChange}>
         Title:
       </InputWithLabel>
